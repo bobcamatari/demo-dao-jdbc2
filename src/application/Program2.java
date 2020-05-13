a795a115e7cf2216675abfec775b3de4b6573b88
bobcamatari <bob.365consultoria@gmail.com>
2020-05-13 14:25:39 -0300

    implemnets departmentFindByID and departmentUpdate

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,6 +14,14 @@ public class Program2 {
 		Department department = new Department(null, "Artes plasticas");
 		depDao.insert(department);
 		System.out.println("Nova inserção. ID: " + department.getId());
+		
+		System.out.println("\n=== TESTE 2: department update ===");
+		int id = 6;
+		department = depDao.findById(id);
+		department.setName("Vegan");
+		depDao.update(department);
+		System.out.println("Update completed");
+		
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -60,7 +60,24 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "+
+					"SET Name = ? "+
+					"WHERE Id = ?" ,
+					Statement.RETURN_GENERATED_KEYS
+					);
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+			
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -72,14 +89,43 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement( 
+					"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				return dep;
+			}
+			return null;
+			
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+		}
+		
 	}
 
 	@Override
 	public List<Department> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+		
 	}
 
 }
